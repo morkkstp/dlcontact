5ba95ef063e679fee508dbc176993c435da1f906
Setthap0ng <109269853+morkkstp@users.noreply.github.com>
2024-11-06 15:31:18 +0700

    ปกติ 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
 import React, { useState, useEffect } from "react";
-import Kasi from "../src/assets/kasi.jpg"; // รูปภาพที่ต้องการแปลง
+
 const App: React.FC = () => {
-  const [base64Image, setBase64Image] = useState<string | null>(null);
+
   const data = {
     name: "Kasidit Boonchai",
     email: "kasidit.bc@gmail.com",
     phone: "084-699-5862",
     address: "Nakkon Si Thammarat, Thailand",
   };
-  // ฟังก์ชันแปลงรูปภาพเป็น Base64
-  const loadImageAsBase64 = () => {
-    const img = new Image();
-    img.src = Kasi; // ระบุ path ของรูปภาพ
-    img.onload = () => {
-      const canvas = document.createElement("canvas");
-      const ctx = canvas.getContext("2d");
-      if (ctx) {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
-        const base64 = canvas.toDataURL("image/jpeg");
-        setBase64Image(base64); // บันทึก Base64 ของรูปภาพ
-      }
-    };
-  };
+
   const handleDownload = () => {
-    if (!base64Image) {
-      alert("กรุณารอให้รูปภาพโหลดเสร็จแล้ว");
-      return;
-    }
+
     const vCard = `
 BEGIN:VCARD
 VERSION:3.0
@@ -36,9 +18,6 @@ FN:${data.name}
 TEL:${data.phone}
 EMAIL:${data.email}
 ADR:${data.address}
-PHOTO;ENCODING=BASE64:${
-      base64Image.split(",")[1]
-    }  // แยกส่วน Base64 ที่ต้องการ
 END:VCARD
     `.trim();
     const blob = new Blob([vCard], { type: "text/vcard" });
@@ -58,9 +37,7 @@ END:VCARD
       URL.revokeObjectURL(url);
     }
   };
-  useEffect(() => {
-    loadImageAsBase64();
-  }, []);
+
   return (
     <button
       onClick={handleDownload}
